820d7e8a1237b74612ab084d7f2ddf2bd8648f6b
abhipatel273 <bhalaniabhi1999@gmail.com>
2022-12-29 23:50:51 +0530

    gfg
    hi  fd

--- src/App.js
+++ src/App.js
@@ -2,31 +2,40 @@
 import "./App.css";
 import Navbar from "./componet/Navbar";
 import Textform from "./componet/Textform";
-import * as React from "react";
+// import * as React from "react";
 import Accordion from "./componet/Accordion";
-import * as ReactDOM from "react-dom";
+// import * as ReactDOM from "react-dom";
 
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
+// import {
+//   createBrowserRouter,
+//   createRoutesFromElements,
+//   Route,
+//   RouterProvider,
+// } from "react-router-dom";
 
-const router = createBrowserRouter(
-  createRoutesFromElements([
-    <Route path="/" element={<Accordion />}></Route>,
-    <Route path="/acc" element={<Navbar />}></Route>,
-    <Route path="/acc/arr" element={<Textform />}></Route>,
-  ])
-);
+// const router = createBrowserRouter(
+//   createRoutesFromElements([
+//     <Route path="/" element={<Accordion />}></Route>,
+//     <Route path="/acc" element={<Navbar />}></Route>,
+//     <Route path="/acc/arr" element={<Textform />}></Route>,
+//   ])
+// );
 
 function App() {
-  return ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
-  );
+  return(
+  // return ReactDOM.createRoot(document.getElementById("root")).render(
+  //   <React.StrictMode>
+  //     <RouterProvider router={router} />
+  //   </React.StrictMode>
+  // );
+  <>
+    <div className="container">
+      {/* <Textform /> */}
+      <Accordion />
+      {/* <Navbar /> */}
+    </div>
+  </>
+  )
 }
 
 export default App;
